7aa7a581f4a1ac15f9a37bb43c0b4b2a3da74df2
Sergio Benavides <sgbenavidesa@gmail.com>
2025-10-01 15:53:26 -0600

    feat: add backend link service

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,7 +1,8 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
+import {App} from "@/server/app";
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
-		return new Response('Hello World!');
+		return App.fetch(request);
 	}
 }
